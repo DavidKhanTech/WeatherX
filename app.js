903a44de467958b10b001e6d5a9a72a2c2429b5b
DavidKhanTech <61749995+DavidKhanTech@users.noreply.github.com>
2020-06-10 12:27:41 -0400

    Connected API and was able to log on console

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-//
+//-----------------------API JS---------------------------------------//
+
+const DOMAIN = "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID";
+const apiKey = "07f242a168c1acc0be2f290d6b332cde"
+
+const searchButton = document.querySelector('.searchButton');
+const cityText = document.querySelector('input');
+const city = document.querySelector('.city');
+const temp = document.querySelector('.temp');
+
+async function getTheWeather() {
+
+  let response = await axios.get(`${DOMAIN}${cityText.input}&units=imperial&appid=${apiKey} `);
+  console.log(response)
+
+
+}
+
+searchButton.addEventListener('click', getTheWeather)
