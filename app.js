f42e05effa309e0f971521ccfdcd4977f66dea55
DavidKhanTech <61749995+DavidKhanTech@users.noreply.github.com>
2020-06-10 19:46:41 -0400

    More Style

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //-----------------------API JS---------------------------------------//
 
-const DOMAIN = "api.openweathermap.org/data/2.5/weather?q=NewYork,ny&APPID=";
+const DOMAIN = "http://api.openweathermap.org/data/2.5/weather?q=NewYork,ny&APPID=";
 const apiKey = "07f242a168c1acc0be2f290d6b332cde"
 
 const searchButton = document.querySelector('.searchButton');
@@ -10,7 +10,7 @@ const temp = document.querySelector('.temp');
 
 async function getTheWeather() {
 
-  let response = await axios.get(`${DOMAIN}${cityText.input}&units=imperial&appid=${apiKey} `);
+  let response = await axios.get(`${DOMAIN}${cityText.input}&units=imperial&appid=${apiKey}`);
   console.log(response)
 
   //   let cityTemp = response.data.city.temp
@@ -19,3 +19,11 @@ async function getTheWeather() {
 }
 
 searchButton.addEventListener('click', getTheWeather)
+
+//-----------------Hidden-------------
+$(document).ready(function () {
+  // Hide the div
+  $(".showMe").hide();
+  // Show the div after 5s
+  $(".showMe").delay(5000).fadeIn(100);
+});
