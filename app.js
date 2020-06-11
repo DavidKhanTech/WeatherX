2f155b36b14df6e094dbba0d1c154e5fc7876c8e
DavidKhanTech <61749995+DavidKhanTech@users.noreply.github.com>
2020-06-11 13:29:44 -0400

    Appended Temperature as an h2

--- app.js
+++ app.js
@@ -1,23 +1,20 @@
 //-----------------------API JS---------------------------------------//
 const searchButton = document.querySelector('#srchBtn');
-// const cityInput = document.querySelector('input');
 const zipCode = document.querySelector('.zip');
-// const temp = document.querySelector('.temp');
-
-
-
-// const DOMAIN = `http://api.openweathermap.org/data/2.5/weather?zip=${zipCode.value}`;
+const weather = document.querySelector('.weather')
 const apiKey = "07f242a168c1acc0be2f290d6b332cde"
 
 async function getTheWeather(e) {
   e.preventDefault()
   const DOMAIN = `http://api.openweathermap.org/data/2.5/weather?zip=${zipCode.value}`;
   let response = await axios.get(`${DOMAIN}&units=imperial&appid=${apiKey}`);
-  console.log(response)
-
-  let temp = response.data.main.temp;
-  temp.innerHTML = temp + "° F";
-
+  // console.log(response)
+  let temp = response.data.main.temp
+  // console.log(temp)
+  let showTemp = document.createElement("h2")
+  let textTemp = document.createTextNode(temp + "° F");
+  weather.append(showTemp)
+  showTemp.append(textTemp)
 }
 
 searchButton.addEventListener('click', getTheWeather)
